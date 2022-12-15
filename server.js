9da291703504c0a5fb70d04d76a820506122ab37
mdang9356@my.sullivan.edu <mdang9356@my.sullivan.edu>
2022-12-14 22:05:26 -0500

    Re-adding app.listen command

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.use(express.urlencoded({extended: false}))
 app.use("/api/balance",balanceRouter)
 app.use("/api/transactions", transactionRouter)
 
-// app.listen(5000, ()=>{
-//     console.log("Server is started on 5000")
-// })
+app.listen(5000, ()=>{
+    console.log("Server is started on 5000")
+})
